43adc60cdd7b70c9b9b20168b6cbf08d9ec63e7d
elegardo <1lagodich@mail.ru>
2023-11-06 14:24:38 +0300

    123

--- 1_sem/Lb4/Lb4/List.h
+++ 1_sem/Lb4/Lb4/List.h
@@ -1,1 +1,35 @@
 #pragma once
+
+template <typename T>
+class Node
+{
+public:
+	T data;
+	Node *next;
+	Node(const T &value) : data(value), next(nullptr) {};
+
+};
+
+template <typename T>
+class List
+{
+private:
+	Node<T> *head;
+	Node<T> *tail;
+public:
+	List() : head(nullptr), tail(nullptr) {};
+	void push(const T &value)
+	{
+		Node<T>* tmp = new Node<T>(value);
+		if (head == nullptr)
+		{
+			head = tmp;
+			tail = tmp;
+		}
+		else
+		{
+			tmp->next = head;
+			head = tmp;
+		}
+	}
+};
