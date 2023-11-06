690386150a5f91ec761a10d15f9f397ade4bff47
elegardo <1lagodich@mail.ru>
2023-11-06 18:55:04 +0300

    123

--- 1_sem/Lb4/Lb4/Lb4.cpp
+++ 1_sem/Lb4/Lb4/Lb4.cpp
@@ -13,6 +13,25 @@ int main()
     IntList.push(13);
     cout << "List with int type: ";
     IntList.print();
-    cout << "Hello world" << endl;
+    IntList.pop();
+    cout << "List after pop: ";
+    IntList.print();
+    IntList.pop_back();
+    cout << "List after pop_back: ";
+    IntList.print();
+    cout << endl;
+
+    List<string> StringList;
+    StringList.push("tail");
+    StringList.push("middle");
+    StringList.push("head");
+    cout << "List with string type: ";
+    StringList.print();
+    StringList.pop();
+    cout << "List after pop: ";
+    StringList.print();
+    StringList.pop_back();
+    cout << "List after pop_back: ";
+    StringList.print();
     return 0;
 }
