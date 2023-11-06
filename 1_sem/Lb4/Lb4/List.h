40c17a199dc3d4e3ad272cdc5867afde8025d7d5
elegardo <1lagodich@mail.ru>
2023-11-06 18:05:48 +0300

    123

--- 1_sem/Lb4/Lb4/List.h
+++ 1_sem/Lb4/Lb4/List.h
@@ -49,4 +49,57 @@ public:
 			return head->data;
 		return NULL;
 	}
+	void push_back(const T& value)
+	{
+		Node<T>* tmp = new Node<T>(value);
+		if (tail == nullptr)
+		{
+			head = tmp;
+			tail = tmp;
+		}
+		else
+		{
+			tail->next = tmp;
+			tail = tmp;
+		}
+	}
+	void pop_back()
+	{
+		if (head != nullptr)
+		{
+			if (head == tail)
+			{
+				delete head;
+				head = nullptr;
+				tail = nullptr;
+			}
+			else
+			{
+				Node<T>* tmp = head;
+				while (tmp->next != tail)
+				{
+					tmp = tmp->next;
+				}
+				delete tail;
+				tail = tmp;
+				tail->next = nullptr;
+			}
+		}
+	}
+	T peek_back()
+	{
+		if (tail != nullptr)
+			return tail->data;
+		return NULL;
+	}
+	void print()
+	{
+		Node<T>* tmp = head;
+		while (tmp != nullptr)
+		{
+			cout << tmp->data << " ";
+			tmp = tmp->next;
+		}
+		count << endl;
+	}
 };
